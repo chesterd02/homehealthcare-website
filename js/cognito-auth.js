595fb85f8684fa109b1ce228780f910ecc56335e
RyanKWest <west.ryan.k@gmail.com>
2020-11-05 16:14:28 -0700

    Add App.session in cognito-auth
    App.session will be populated with the session that is inside the
    authToken. That way, it is accessible from every page.

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -47,6 +47,24 @@ var App = window.App || {};
         }
     });
 
+    function parseJwt (token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    };
+
+    App.authToken
+        .then(function setSession(jwtToken) {
+            App.session = parseJwt(jwtToken);
+        })
+        .catch(function handleError() {
+            App.session = null;
+        });
+
     /*
      * Cognito User Pool functions
      */
